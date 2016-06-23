2cbf5d945aa5fd40a9df88c83fdc119b38430681
Sergiu Buciuc <sbuciuc@axway.com>
2016-06-23 13:17:41 +0300

    proper assert in unit test

--- test/directionsService.spec.js
+++ test/directionsService.spec.js
@@ -1,8 +1,16 @@
 const directionsService = require('../services/directionsService');
+const expect = require('chai').expect;
+
 describe('DirectionsService', () => {
   it('returns a path between two coordinates', (done) => {
     directionsService.getPath({ origin: 'Bucharest, Romania', destination: 'Moscow, Russia' })
       .then((path) => {
+        const firstPoint = path[0];
+        const lastPoint = path[path.length - 1];
+        const [expectedFirstPoint, expectedLastPoint] = [[ 44.42659, 26.10278 ], [ 55.75615, 37.6172 ]];
+
+        expect(firstPoint).to.deep.equal(expectedFirstPoint);
+        expect(lastPoint).to.deep.equal(expectedLastPoint);
         done();
       })
       .catch(done);
